7a3874b0a07e6cf1f4c43583ca2900c51f0a6937
phpgogogo <cliff60309@gmail.com>
2021-12-30 11:31:27 +0800

    增加方法2:使用後端方式產生網頁內容

--- app2/api/list.php
+++ app2/api/list.php
@@ -7,4 +7,15 @@ $rows=$Stu->q($sql);
 // }
 echo json_encode($rows);
 
+// 在後台整理好資料再輸出到頁面
+// $list="";
+// foreach($rows as $row){
+//     $list.="<div class='card'>";
+//     $list.="<div class='card-body'>";
+//     $list.="<h5 class='card-title'>{$row['班級']}</h5>";
+//     $list.="<p class='card-text'>{$row['人數']}</p>";
+//     $list.="</div>";
+//     $list.="</div>";
+// }
+// echo $list;
 ?>
